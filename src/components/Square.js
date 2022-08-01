e5f0cd85dfd8df944f35184c41587eaec9ba99ab
BastD29 <bastiendaniel9@gmail.com>
2022-08-01 16:23:02 +0200

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,32 @@
+// import React, { Component } from 'react'
+
+// export default class Square extends Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             value: null,
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <button
+//                 className='square'
+//                 onClick={() => this.props.onClick()}
+//             >
+//                 {this.props.value}
+//             </button>
+//         )
+//     }
+// }
+
+export default function Square(props){
+    return (
+        <button 
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
+}
